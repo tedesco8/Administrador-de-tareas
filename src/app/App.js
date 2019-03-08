d8f42c9a47f632bdb2cefa230f71056153e4a0ac
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-03-08 00:17:36 -0300

    formulario tarea

--- src/app/App.js
+++ src/app/App.js
@@ -5,11 +5,34 @@ class App extends Component {
     render(){
         return (
             <div>
+                {/*Navegación*/}
                 <nav className="light-blue darken-4">
                 <div className="container">
-                <a className="brand-logo" href="/">MERN Stack</a>
+                <a className="brand-logo" href="/">Administración de Tareas</a>
                 </div>
                 </nav>
+                {/*Contenido*/}
+                <div className="container">
+                     <div className="row">
+                         <div className="col s5">
+                             <div className="card">
+                                <div className="card-content">
+                                    <form>
+                                        <div className="row">
+                                           <div className="input-field col s12">
+                                              <input type="text" placeholder="Titulo Tarea" />
+                                           </div>
+                                        </div>
+                                    </form>
+                                </div>
+                             </div>
+                         </div>
+                         <div className="col s7">
+
+                         </div>
+                     </div>
+                </div>    
+
             </div>
             )
     }
